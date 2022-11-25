e02a49df2f43a3a836dc575c45b498422cd5e6f9
Victoria Gardella <v.gardella.ruiz@gmail.com>
2022-11-25 13:18:16 -0300

    Define function to get player's selection

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ function getComputerChoice() {
     let index = Math.floor(Math.random() * choices.length);
     return choices[index];
 }
+
+function playerSelection() {
+    let selection = prompt('Choose rock, paper or scissors:');
+    selection = selection.charAt(0).toUpperCase() + selection.slice(1);
+    return selection
+}
+

b8c7cf6f2c37b648550e66de2d26039b3fbbf719
Victoria Gardella <v.gardella.ruiz@gmail.com>
2022-11-25 13:18:48 -0300

    Define function to play a round

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ function playerSelection() {
     return selection
 }
 
+function playRound(computerChoice, playerChoice) {
+    if (playerChoice === '') {
+        console.log(`You have to choose an option!`);
+    } else if (playerChoice !== 'Rock' && playerChoice !== 'Paper' && playerChoice !== 'Scissors') {
+        console.log(`Chose one of the accepted options!`);
+    } else if (computerChoice === playerChoice) {
+        console.log(`You and the computer choose ${computerChoice}; Is\'s a tie!`);
+    } else if (computerChoice === 'Rock' && playerChoice === 'Scissors' ||
+    computerChoice === 'Paper' && playerChoice === 'Rock' ||
+    computerChoice === 'Rock' && playerChoice === 'Scissors') {
+        console.log(`Computer\'s choice: ${computerChoice} \n Your choice: ${playerChoice}; You lost!`);
+    } else {
+        console.log(`Computer\'s choice: ${computerChoice} \n Your choice: ${playerChoice}; You won!`);
+    }
+}
+

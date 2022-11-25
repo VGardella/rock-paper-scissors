e3fb245c88b2e12a86190cc168b0c3fed07b4a30
Victoria Gardella <v.gardella.ruiz@gmail.com>
2022-11-25 13:17:40 -0300

    Define function for computer choice

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('Hello World')
+function getComputerChoice() {
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    let index = Math.floor(Math.random() * choices.length);
+    return choices[index];
+}

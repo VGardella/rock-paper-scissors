8e555fab9577dc49dd5c7e1b25b05dd0f64b966b
Victoria Gardella <v.gardella.ruiz@gmail.com>
2022-12-02 12:45:55 -0300

    Rename file

--- index.js
+++ index.js
@@ -1,42 +1,65 @@
+// functions
 function getComputerChoice() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
-    let index = Math.floor(Math.random() * choices.length);
-    return choices[index];
+  const choices = ['Rock', 'Paper', 'Scissors'];
+  let index = Math.floor(Math.random() * choices.length);
+  return choices[index];
 }
 
 function playRound(computerChoice, playerChoice) {
-    
-    //results
-    const results = document.getElementById('results');
-    results.style.cssText = 'border: 2px solid black; height: 100px; width: 200px; margin: 30px';
+  //results
+  const results = document.getElementById('results');
+  results.style.cssText = 'border: 2px solid black; height: 100px; width: 200px; margin: 30px';
 
-    
-    if (computerChoice === playerChoice) {
-        results.textContent = `You and the computer choose ${computerChoice}; Is\'s a tie!`;
-    } 
-    else if (computerChoice === 'Rock' && playerChoice === 'Scissors' ||
-    computerChoice === 'Paper' && playerChoice === 'Rock' ||
-    computerChoice === 'Scissors' && playerChoice === 'Paper') {
-        results.textContent = `Computer\'s choice: ${computerChoice} \n Your choice: ${playerChoice}; You lost!`;
-    } 
-    else {
-        results.textContent = `Computer\'s choice: ${computerChoice} \n Your choice: ${playerChoice}; You won!`;
-    }
+  if (computerChoice === playerChoice) {
+      results.textContent = `You and the computer choose ${computerChoice}; Is\'s a tie!`;
+      return winner = 0;
+  } 
+  else if (computerChoice === 'Rock' && playerChoice === 'Scissors' ||
+  computerChoice === 'Paper' && playerChoice === 'Rock' ||
+  computerChoice === 'Scissors' && playerChoice === 'Paper') {
+      results.textContent = `Computer\'s choice: ${computerChoice} \n Your choice: ${playerChoice}; You lost!`;
+      return winner = 'computer';
+  } 
+  else {
+      results.textContent = `Computer\'s choice: ${computerChoice} \n Your choice: ${playerChoice}; You won!`;
+      return winner = 'player';
+  }
 }
 
 
-function game() {
+// game
+
+let playerChoice = 0;
+let winner = 0;
+let playerWins = 0;
+let computerWins = 0;
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", function() {
     let computerChoice = getComputerChoice();
+    let playerChoice = this.id.charAt(0).toUpperCase() + this.id.slice(1).toLowerCase();
+    playRound(computerChoice, playerChoice);
+    if (winner === 'player') {
+      playerWins += 1;
+    }
+    if (winner === 'computer') {
+      computerWins += 1;
+    }
+    if (playerWins === 5) {
+      results.textContent = 'Player Wins!';
+      playerWins = 0;
+      computerWins = 0;
+    }
+    else if (computerWins === 5) {
+      results.textContent = 'Computer Wins!';
+      playerWins = 0;
+      computerWins = 0;
+    }
+    console.log(playerWins);
+    console.log(computerWins);
+    console.log(winner)
+  });
+  });
 
-    //buttons and listeners
-    const rock = document.querySelector('#rock');
-    rock.addEventListener('click', () => {playRound(getComputerChoice(), 'Rock');});
-
-    const paper = document.querySelector('#paper');
-    paper.addEventListener('click', () => {playRound(getComputerChoice(), 'Paper');});
-
-    const scissors = document.querySelector('#scissors');
-    scissors.addEventListener('click', () => {playRound(getComputerChoice(), 'Scissors');});
-}
-
-game()
